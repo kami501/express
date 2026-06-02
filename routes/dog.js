e77eca4e9cd555342f446228f14e83a46a3dba00
kami <https://www.google.com/support/accounts/bin/answer.py?answer=181692>
2026-06-02 21:10:57 +0900

    add dog api

--- routes/dog.js
+++ routes/dog.js
@@ -1,11 +1,11 @@
-varexpress= require('express');
-varrouter= express.Router();
-constrequest= require('request');
+var express= require('express');
+var router= express.Router();
+const request= require('request');
 router.get('/', async(req, res) =>{
 request('https://dog.ceo/api/breeds/image/random', function(error, response, 
 body) {
 if(!error&& response.statusCode== 200) {
-constdata= JSON.parse(body);
+const data= JSON.parse(body);
 res.json(data);
 }
 });
